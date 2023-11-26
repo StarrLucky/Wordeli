c90e3b5f4df27a464bbd85933c23b0ad164a97db
Ivan Kuznetsov <fxndstrs@gmail.com>
2023-11-26 21:55:00 +0400

    Update post.sh

--- post.sh
+++ post.sh
@@ -9,7 +9,7 @@ newPhrase=$(/usr/local/bin/python3 /wordeli/wordeli.py)
 echo ""
 echo $newPhrase
 
-curl -X POST "https://api.telegram.org/bot6731602403:AAFpYkjxxdRuq8lMiDt7Rid4NYoNPQQHlkU/sendMessage" -d "chat_id=$2&text=$newPhrase"
+curl -X POST "https://api.telegram.org/$1/sendMessage" -d "chat_id=$2&text=$newPhrase"
 
 # dictWord=$(/usr/local/bin/python3 /wordeli/stardict.py)
 # echo ""
